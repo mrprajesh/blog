23313739a284c97e25cac8da6cc2b439cb2d1f00
Rajesh Pandian M <mrpraj@gmail.com>
2020-03-30 15:49:30 +0530

    Auto updated!  - src

--- publish.sh
+++ publish.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
+
 GITHUB=../mrprajesh.github.io/blog
+
+TSTAMP=${date +'%d-%b-%Y-%T'}
 
 # commit src jekyll files
 git add .
-git commit -m "Auto updated! src"
+git commit -m "Auto updated! $TSTAMP - src"
 git push origin master
 
 # copy files to local repo then commit and push html files
 rsync -arvh  _site/ "$GITHUB"
 cd "$GITHUB"
 git add .
-git commit -m "Auto updated! - blog"
+git commit -m "Auto updated! $TSTAMP - blog"
 git push origin master
 cd -
+
+echo $TSTAMP - Done!
 
